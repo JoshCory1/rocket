6121d69a9953e37eb75cf9481301e339b39d982e
Joshua Cory <accounts@keeplooking.net>
2023-12-05 10:02:23 -0700

    update movement to local with basis.y

--- Prefabs/Player.gd
+++ Prefabs/Player.gd
@@ -1,10 +1,10 @@
-extends Node3D
+extends RigidBody3D
 
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+		apply_central_force(basis.y * delta * 1000)
+	if Input.is_action_pressed("Left_A"):
+		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+	if Input.is_action_pressed("Right_D"):
+		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
